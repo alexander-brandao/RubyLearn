6a6d5803a389b465d39e7e0caa9f8b83731302b0
Alexander Brandao <alex12a7x@gmail.com>
2021-06-16 16:37:21 -0300

    array and parameter

--- class.rb
+++ class.rb
@@ -49,7 +49,7 @@ end
 ["laranja","maçã","uva"].each {|fruta| puts fruta}
 
 #como imprimir do 0 até o 100 com a funcao ".upto"
-0.upto(100) {|i| puts i}
+0.upto(10) {|i| puts i}
 
 #convertendo uma string em um array
 lista = %w(conversao de string para array)
@@ -57,6 +57,12 @@ lista = %w(conversao de string para array)
 #criando uma tabela hash com chave e valor
 hash = {nome: 'Alexander', idade: '25'}
 
+# '*' transforma os parametros da funcao em arrays exemplo:
+ def imprimir_nomes(*nomes)
+    nomes.each {|n| puts n }
+ end
+#executando a funcao
+ imprimir_nomes 'bruno','paulinho'
 
 
 
