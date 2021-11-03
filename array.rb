e12587f300b26e3c5c62372e57de928269a507c5
Alexander Brandao <alex12a7x@gmail.com>
2021-11-03 17:20:16 -0300

    master array

--- array.rb
+++ array.rb
@@ -7,6 +7,16 @@ lista << 1.1
 outra_lista = Array.new
 
 outra_lista = [1,2,"Alex", :simbolo, /$regexˆ/]
-puts outra_lista[0]
+#puts outra_lista[0]
+
+def compra(*produtos)
+    puts produtos.size
+    puts produtos[0]
+end
+
+compra("macbook","mustang GT", "Abadá")
+
+
+
 
 
