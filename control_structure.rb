89d5a957cb89fda76593b87e8a062281a1eccc09
Alexander <alex12a7x@gmail.com>
2021-12-30 17:33:07 -0300

    funcao copa

--- control_structure.rb
+++ control_structure.rb
@@ -1,4 +1,23 @@
-3 > 3
-#pagina 21 apostila
-def nao
-end
+#switch 
+
+def procura_sede_copa_mundo(ano)
+    case ano
+        when 1895..2005
+            "não lembro"
+        when 2006
+            "aleamnha"
+        when 2010
+            "africa do sul"
+        when 2014
+            "brasil"
+        when 2018
+            "russia"
+        when 2022
+            "Qatar"
+        else
+            "indefinido"
+    end
+end 
+
+
+puts procura_sede_copa_mundo(2009)
