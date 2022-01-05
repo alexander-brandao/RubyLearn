c859616d8a251422dcd6bd35c20d56bacd614300
Alexander <alex12a7x@gmail.com>
2022-01-05 19:45:00 -0300

    classes

--- classes.rb
+++ classes.rb
@@ -1,42 +1,11 @@
-class Restaurante
-    def qualifica(nota, msg="obrigado")
-        print "A nota do restaurante foi #{nota}. #{msg}  \n" 
+class Restaurante 
+    def qualifica(nota, msg = "thank you")
+        
+        puts "your grade was #{nota}. #{msg}"
+        
     end
 end
 
-#restaurante = Restaurante.new
-#restaurante.qualifica('10')
+giraffas = Restaurante.new
 
-class Pessoa
-
-    def initialize
-        puts "criando uma nova pessoa"
-    end
-
-    def muda_nome(novo_nome)
-        @nome = novo_nome
-    end
-
-    def diz_nome
-       puts "meu nome é #{@nome}"
-    end
-end
-
-#p = Pessoa.new
-#p.muda_nome("Alexander")
-#p.diz_nome
-
-class Carro
-    def nome
-       @nome 
-    end 
-
-    def nome=(novo_nome)
-        @nome = novo_nome
-    end
-end
-
-#c = Carro.new
-#c.nome=("Alexander")
-#puts c.nome
-
+giraffas.qualifica(10)
