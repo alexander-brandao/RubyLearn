22a19afea046ef665aba940092e40351adb2be5a
Alexander <alex12a7x@gmail.com>
2022-01-06 17:27:11 -0300

    initialize

--- classes.rb
+++ classes.rb
@@ -1,11 +1,17 @@
-#class Restaurante 
-#   def qualifica(nota, msg = "thank you")
+class Restaurante 
+
+    def initialize
+        puts "criando um novo restaurante"
+    end
+
+    #   def qualifica(nota, msg = "thank you")
 #        
 #        puts "your grade was #{nota}. #{msg}"
         
 #    end
-#end
+end
 
+Restaurante.new
 #giraffas = Restaurante.new
 
 #giraffas.qualifica(10)
