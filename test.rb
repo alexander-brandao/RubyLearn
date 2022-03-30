ebce490603e27bb01775e6f9ca3f98107e0a4eed
AlexLinux <alex12a7x@gmail.com>
2022-03-30 16:30:43 -0300

    multiplos parametros

--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
+=begin 
 lista = Array.new
 lista << "Alexander, "
 lista << "Vai superar "
@@ -6,3 +7,16 @@ lista << "vencer o pecado "
 lista << "e Reinar com o senhor "
 
 puts lista
+=end
+
+=begin
+lista = [1,2, "string", :simbolo, /$regex^/]
+puts lista
+=end
+
+def compra(*produtos)
+    puts produtos.size
+end
+
+compra("panetone", "cueca", "carro")
+
