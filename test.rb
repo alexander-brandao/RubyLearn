7a5da6238953b37db4207511a1f3c8de82e7a9bd
AlexLinux <alex12a7x@gmail.com>
2022-03-30 16:41:07 -0300

    hashes

--- test.rb
+++ test.rb
@@ -14,9 +14,19 @@ lista = [1,2, "string", :simbolo, /$regex^/]
 puts lista
 =end
 
+=begin
 def compra(*produtos)
     puts produtos.size
 end
 
 compra("panetone", "cueca", "carro")
+=end
 
+#tabela hash
+config = Hash.new
+    config["porta"] = 80
+    config["ssh"] = false
+    config["nome"] = "google.com"
+
+    puts config.size
+    puts config["ssh"]
