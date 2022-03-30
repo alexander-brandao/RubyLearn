0319379108365fc20a3ba514466d37f2eac7990a
AlexLinux <alex12a7x@gmail.com>
2022-03-30 16:19:10 -0300

    seguindo a apostila

--- test.rb
+++ test.rb
@@ -1,3 +1,13 @@
-puts "teste"
-ano = 1998
-puts ano 
+class Pessoa
+    def muda_nome(novo_nome)
+        @nome = novo_nome
+    end
+
+    def diz_nome
+        puts "meu nome é #{@nome}"
+    end
+end
+
+pessoa = Pessoa.new
+pessoa.muda_nome "Joao"
+pessoa.diz_nome
